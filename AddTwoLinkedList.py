b084ba822e19677244e4192f6751d8656117c475
Jonathan Baumert <jonathanbaumert@yahoo.com>
2020-05-16 00:04:19 -0700

    add function to create a LinkedList from an array, add test cases

--- AddTwoLinkedList.py
+++ AddTwoLinkedList.py
@@ -6,19 +6,29 @@ class ListNode:
         self.val = val
         self.next = next
 
+def createLinkedListFromArray(arrayIn: list) -> ListNode:
+    linkedList = list()
+    linkedList.append((ListNode(arrayIn[0])))
+    for i in range(1,len(arrayIn)):
+        linkedList.append(ListNode(arrayIn[i]))
+        linkedList[i-1].next = linkedList[i]
+
+    return linkedList[0]
+
+
 def addTwoNumbers(l1: ListNode, l2: ListNode) -> ListNode:
     values = [l1.val,l2.val]
     i = 1
     nextNode1 = l1.next
     while nextNode1 != None:
-        values[0] = values[0] * 10 + nextNode1.val
+        values[0] = values[0] + nextNode1.val * (10 ** i)
         i = i+1
         nextNode1 = nextNode1.next
         
     i = 1
     nextNode2 = l2.next
     while nextNode2 != None:
-        values[1] = values[1] * 10 + nextNode2.val
+        values[1] = values[1] + nextNode2.val * (10 ** i)
         i = i + 1
         nextNode2 = nextNode2.next
         
@@ -52,3 +62,10 @@ onesNode2 = ListNode(5, tensNode2)
 
 o = addTwoNumbers(onesNode1,onesNode2)
 print(f"{o.val}, {o.next.val}, {o.next.next.val}")
+
+tensNode3 = ListNode(8)
+onesNode3 = ListNode(1, tensNode3)
+
+onesNode4 = ListNode(0)
+o2 = addTwoNumbers(onesNode3, onesNode4)
+print(f"{o2.val}, {o2.next.val}")
